6ce00484682c5a75fa12fdcb8fae194f69c3ede9
see09150 <see09150@castleton.edu>
2022-09-28 10:39:47 -0400

    Adding arrays and for loops

--- question-1-2-3-4.js
+++ question-1-2-3-4.js
@@ -3,14 +3,14 @@
 //Below this line, fill in the array named "assignments" with the string values
 //from question one in the lab.
 
-let assignments = [        ];
+let assignments = [ "Lab 1" , "Lab 2" , "Mid Term" , "Lab 3" , "Project" , "Final Exam"       ];
 
 
 //❓❓ LAB QUESTION 2:
 //Below this line, declare an array named "grades" with the numeric values
 //from question one in the lab.
 
-
+let grades = [ 90, 80, 78, 81, 91, 89]
 
 
 //❓❓ LAB QUESTION 3:
@@ -18,12 +18,16 @@ let assignments = [        ];
 //from question one in the lab.
 console.log("Grades:");
 for ( let i = 0; i < assignments.length; i++ ){
-    console.log(assignments[i], "????");
+    console.log(assignments[i], grades [i]);
 }
 
 
 //❓❓ LAB QUESTION 4:
 //Calculate the average grade and print it out:
 
-
+let sum = 0
+for ( let a = 0; a < assignments.length ; a++){
+    sum = sum + grades[a]
+}
+console.log("Average Grade:" , sum / grades.length)
 
--- question-6-7-8.js
+++ question-6-7-8.js
@@ -12,7 +12,11 @@ let dictionary = [
 //a haystack?).
 //Return true when you find it, and false if you do not.
 function linearSearch(needle, haystack){
-    
+    for (let i = 0; i < haystack.length; i ++){
+        if (haystack[i] == needle)
+            return true;
+    } 
+    return false
 }
 
 console.log("Does it contain cat? ", linearSearch("cat", dictionary));
